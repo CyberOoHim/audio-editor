feb96d259c35d6137ebf1633607cdc7f0ef1c74b
CyberOoHim <125890630+CyberOoHim@users.noreply.github.com>
2026-08-21 13:47:30 +0800

    feat: some adjustments for Google AI Studio import

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   base: './',
   build: {
     target: 'esnext',
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false
-  },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'anonymous'
-    }
   }
 })
